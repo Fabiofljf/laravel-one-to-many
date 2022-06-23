df22055c56f69857db630bcced7ea8bcbb5b5a1e
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-23 16:21:46 +0200

    Add relationship one to many

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,15 +1,27 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $fillable = ['Programming', 'Automation', 'Web design', 'Best Practices'];
 
-    public static function generateSlug($title)
+    public static function generateSlug($name)
     {
-        return Str::slug($title, '-');
+        return Str::slug($name, '-');
+    }
+
+    /**
+     * Get the category that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
     }
 }
